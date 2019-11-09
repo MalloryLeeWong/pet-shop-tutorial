c5576fc3976c75dac7ae35e6e02746841875435e
Mallory LeeWong <m.c.leewong@gmail.com>
2019-11-09 16:58:58 -0500

    feat: smart contract Adoption compiled

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,21 +1,26 @@
-pragma solidity ^0.5.0
+pragma solidity ^0.5.0;
 
 contract Adoption {
   // declaring a public var adopters which is an array of addresses with length 16
-  address[16] public adopters
+  address[16] public adopters;
 
   // adopting a pet
-  function adopt(uint petId) public returns (unit) {
     // require() checks if the petId is within the array index range, since length is 16, must be 0 to 15 indices
-    require(petId >= 0 && petId <= 15)
     // msg.sender is the address of the person or smart contract that called this function
     // save the address as an element in the adopters array at the index equal to the petId
-    adopters[petId] = msg.sender
-    // array getters only return a single value
-    return petId
+
+  function adopt(uint petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
+    adopters[petId] = msg.sender;
+    return petId;
   }
+
+// getting the adopters array
+// include memory keyword gives the address in memory for the var
+// view keyword means func won't modify state of the contract
+  function getAdopters() public view returns (address[16] memory) {
+    return adopters;
+  }
+
 }
 
-function getAdopters() public view returns (address[16] memory) {
-  return adopters
-}
--- truffle-config.js
+++ truffle-config.js
@@ -10,5 +10,11 @@ module.exports = {
     develop: {
       port: 8545
     }
+  },
+  // I added this
+  compilers: {
+    solc: {
+      version: "0.5.0"
+    }
   }
 };
